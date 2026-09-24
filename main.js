a68ffde11f3464845fb77eed1d5494267134d06e
MONEYWAY-maker <lenwoodsbiz@gmail.com>
2026-09-24 02:14:56 -0700

    Updated js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-getValue ={// Write your code below
+Write a function called getValue which takes two parameters:
+
+obj - an object
+key - a string
+The function should return the value that is associated with the given key.
+
+Notes:
+
+If the key is not found in the object, it should return undefined.
+For example:
+
+var obj = {
+  'key': 'value'
+};
+var output = getValue(obj, 'key');
+console.log(output); // should print 'value'
+
+
+
+
+Function getValue = 
+
+var obj = { 'key': 'value
+};
+
+var output = getValue(obj, 'key');
+console.log (output); 
+
